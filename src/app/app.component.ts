68e9087e976cd03ea164313b46d4e8547c6b9c5a
TanjaGagic <57789556+TanjaGagic@users.noreply.github.com>
2022-04-04 21:56:29 +0200

    changed sidebar

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -6,13 +6,11 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   public appPages = [
-    { title: 'Inbox', url: '/folder/Inbox', icon: 'mail' },
-    { title: 'Outbox', url: '/folder/Outbox', icon: 'paper-plane' },
-    { title: 'Favorites', url: '/folder/Favorites', icon: 'heart' },
-    { title: 'Archived', url: '/folder/Archived', icon: 'archive' },
-    { title: 'Trash', url: '/folder/Trash', icon: 'trash' },
-    { title: 'Spam', url: '/folder/Spam', icon: 'warning' },
+    { title: 'Homepage', url: '/folder/Homepage', icon: 'home' },
+    { title: 'Books', url: '/folder/Books', icon: 'book' },
+    { title: 'Movies', url: '/folder/Movies', icon: 'videocam' },
+    { title: 'TV Shows', url: '/folder/TV Shows', icon: 'tv' },
+    { title: 'About us', url: '/folder/About Us', icon: 'human' },
   ];
-  public labels = ['Family', 'Friends', 'Notes', 'Work', 'Travel', 'Reminders'];
   constructor() {}
 }
